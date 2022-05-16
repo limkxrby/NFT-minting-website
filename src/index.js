b5c5adb48dc58e88918f883b5d7e73e004c3715c
buang99 <lim.kirby@yahoo.com>
2022-05-16 18:40:51 -0400

    deploy on heroku

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import './index.css';
 import App from './App';
 
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
