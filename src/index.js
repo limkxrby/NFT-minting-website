3dccf13c042b10193b2689b443786915af4dfaa9
buang99 <lim.kirby@yahoo.com>
2022-05-16 17:31:10 -0400

    make it better

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const express = require('express');
+const app = express();
+
+app.get('./', (req, res){
+  console.log('here')
+  res.sendStatus(500)
+
+}
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
